f10f2a7685f12b3d90853c43a2a9d9cefabe6623
FireFox16 <firefox.2016@yahoo.com>
2019-06-12 07:32:12 -0700

    Create Downloader.py

---
+++ Downloader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import requests
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split('/')[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+
+download("https://i.ytimg.com/vi/GNs8jIraTSk/maxresdefault.jpg")
